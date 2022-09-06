65e5438267f53d30dcd4c9055fe40f290fb3f93d
flashnuke <adanikel@gmail.com>
2022-09-06 05:20:29 -0400

    add cls

--- wificepter.py
+++ wificepter.py
@@ -1,30 +1,20 @@
 from scapy.all import *
 
+class Interceptor:
+    def __init__(
+
 def foo(frame):
     if not frame.haslayer(Dot11):
         return
-     #   if frame.haslayer(Dot11Elt):
-      #      netname = frame[Dot11Elt].info.decode()
-       #     if True or "ff:ff:ff" not in frame[Dot11].addr1 and "ff:ff:ff" not in frame[Dot11].addr2:
-#                print(netname) 
- #               print(frame[Dot11].addr1)
-  #              print(frame[Dot11].addr2)
-   #             print(frame[Dot11].addr3)
-    #    else:
-     #       return
-      #      frame.show()
-    #while elt and elt.ID != 0:
-     #   elt.show()
-       # elt = elt.payload[Dot11Elt]
     try:
         if  "ff:ff:ff" not in frame.addr1 and "ff:ff:ff" not in frame.addr2:
             elt = frame[Dot11Elt]
             if len(elt.info) == 0:
                 return
-            print(elt.info)
-            print(frame.addr1)
+            ssid = print(elt.info)
+            src_device_mac = frame.addr1
             print(frame.addr2)
-            print(frame.addr3)
+            ap_mac = frame.addr3
     except:
         pass
 interface = "wlan0" # todo
