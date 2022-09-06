330d2d08ac1a2f0482af3b0dbc8486d5015909a5
flashnuke <adanikel@gmail.com>
2022-09-06 12:29:31 +0300

    define cls

--- b.py
+++ b.py
@@ -5,7 +5,6 @@ def channel_hopper():
     while True:
         try:
             channel = random.randrange(1,12)
-            os.system("iw dev %s set channel %d" % ("wlan0", channel))
             time.sleep(1)
         except KeyboardInterrupt:
             break
--- wificepter.py
+++ wificepter.py
@@ -1,22 +1,40 @@
 from scapy.all import *
+import os
+
 
 class Interceptor:
-    def __init__(
+    def __init__(self, *args, **kwargs):
+        self.ch_range = range(1, 12)  # todo utils
+        self.interface = "wlan0" # todo set as conf also
+        self._channel_sniff_timeout = 5  # todo utils
 
-def foo(frame):
-    if not frame.haslayer(Dot11):
-        return
-    try:
-        if  "ff:ff:ff" not in frame.addr1 and "ff:ff:ff" not in frame.addr2:
-            elt = frame[Dot11Elt]
-            if len(elt.info) == 0:
-                return
-            ssid = print(elt.info)
-            src_device_mac = frame.addr1
-            print(frame.addr2)
-            ap_mac = frame.addr3
-    except:
-        pass
-interface = "wlan0" # todo
+    def _set_channel(self, ch_num):
+        os.system("iw dev %s set channel %d" % (self.interface, ch_num))
 
-sniff(prn=foo, iface=interface)
+    def _ap_scan_cb(self, pkt):
+        if pkt.haslayer(Dot11):
+            try:
+                if "ff:ff:ff" not in pkt.addr1 and "ff:ff:ff" not in pkt.addr2: # TODO ff:ff:ff into utils
+                    elt = pkt[Dot11Elt]
+                    if len(elt.info) == 0:
+                        return
+                    ssid = print(elt.info)
+                    src_device_mac = pkt.addr1
+                    print(pkt.addr2)
+                    ap_mac = pkt.addr3
+            except:
+                pass
+
+    def _scan_for_aps(self):
+        for ch in self.interface:
+            print(f"setting and scanning channel {ch}...")
+            self._set_channel(ch)
+            sniff(prn=self._ap_scan_cb, iface=self.interface, timeout=self._channel_sniff_timeout)
+
+
+
+
+
+if __name__ == "__main__":
+    i = Interceptor()
+    i._scan_for_aps()
